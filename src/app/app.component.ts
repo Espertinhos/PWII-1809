175fed5286fc9b6210d33757d91c5ec7308008ec
Espertinhos <murillo_1230@hotmail.com>
2019-09-18 21:46:27 -0300

    Event binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,5 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Hello World';
-  foto = 'favicon.ico'
+  foto = 'favicon.ico';
+
+  exibir(){
+      alert(this.title)
+  }
 }
